9535a9bd1952889997012b35a2f0bb41ac02c5f4
AleksandrKorzun <korzun.oleksandr@gmail.com>
2021-10-21 16:25:53 +0300

    DZ3

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import React, { Component } from 'react'
+import Searchbar from './components/Searchbar/Searchbar';
+
+import {getPictures} from './services/api.js'
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Button from './components/Button/Button';
+import Modal from './components/Modal/Modal';
+import Loader from './components/Loader/Loader';
+
+
+class App extends Component {
+  state = { 
+    picture: [],
+    page: 0,
+    search: "",
+    isOpenModal: false, 
+    largeImageURL : "",
+    loader: false
+   }
+   componentDidUpdate(_, prevState) {
+     if (prevState.page !== this.state.page || prevState.search !== this.state.search) {
+      this.onToggleLoader()
+      getPictures(this.state.search, this.state.page).
+      then(data=>this.setState(prev=>({picture: [...prev.picture, ...data]}))).finally(this.onToggleLoader())
+     }
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+   }
+   onHandleSubmit = (e) => {
+     e.preventDefault()
+     
+     const search = e.target.elements[1].value;
+     this.setState({page: 1, search, picture: []})
+    
+   }
+   onHandleMorePicture = (e) => {
+    
+    this.setState(prev=>({page: prev.page += 1}))
+   }
+
+   onToggleModal = (e) => {
+     this.setState((prev) => ({ isOpenModal: !prev.isOpenModal}))
+   }
+   onModalImageUrl = (e) => {
+
+    this.setState({largeImageURL: e.target.id})
+    this.onToggleModal()    
+   }
+   onToggleLoader = () => {
+     this.setState((prev) =>({loader: !prev.loader}))
+   }
+  render() {
+    return (
+      <>
+      <Searchbar 
+        onHandleSubmit={this.onHandleSubmit}
+      />
+      {this.state.loader && <Loader/>}
+      <ImageGallery pictures={this.state.picture} onToggleModal={this.onModalImageUrl}/>
+      {this.state.picture.length !== 0 && <Button getMorePictures={this.onHandleMorePicture}/>}
+      {this.state.isOpenModal && <Modal largeImageURL={this.state.largeImageURL} onToggleModal={this.onToggleModal}/>}
+      </>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+const BASE_URL = "https://pixabay.com/api/"
+const key = "23179954-204c91e2d20c9745fc8192c94"
+
+const getPictures = async (search, page) => {
+    try {
+        
+        const response = await axios.get(`${BASE_URL}?q=${search}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`)
+        console.log(response)
+        return response.data.hits
+    } catch (error) {
+        throw new Error(error.message)
+    }
+
+}
+
+
+export {getPictures}
+
+
+
